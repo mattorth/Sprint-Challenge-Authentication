a4fce9d075010eadac4b402d3fd6cb0d1b57efee
mattorth <orth.mj@gmail.com>
2020-02-23 18:51:03 -0800

    authenticate middleware and auth-router complete

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,57 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
+const authenticate = require('./authenticate-middleware');
+
+const Users = require('../users/users-model');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    then(saved => {
+      const token = genToken(saved);
+      res.status(201).json({ created_user: saved, token: token });
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', authenticate, (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = genToken(user);
+        res.status(200).json({ username: user.username, token: token })
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: err})
+    })
 });
+
+function genToken(user) {
+  const payload = {
+    userid: user.id,
+    username: user.username,
+  };
+
+  const options = { expiresIn: '1h' };
+
+  const token = jwt.sign(payload, secrets.jwtSecret, options);
+
+  return token;
+};
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,25 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/users-model');
 
 module.exports = (req, res, next) => {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' })
+        }
+      });
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
   res.status(401).json({ you: 'shall not pass!' });
 };
