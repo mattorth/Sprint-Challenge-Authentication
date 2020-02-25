bbfc4cf3d294535f33433847b72cdc997d905751
mattorth <orth.mj@gmail.com>
2020-02-24 20:59:55 -0800

    update

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,6 +3,16 @@ const request = require('supertest');
 const server = require('./server');
 const db = require('../database/dbConfig');
 const Users = require('../users/users-model');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const secrets = require('../config/secrets');
+
+const user = {
+    username: "matt",
+    password: "password"
+};
+
+let token;
 
 describe('server.js', () => {
     it('should set testing environment', () => {
@@ -10,42 +20,56 @@ describe('server.js', () => {
     });
 
     describe('Auth', () => {
-        it('should insert user', async () => {
-            await Users.add({username:"matthew", password: "password"});
-            const users = await db('users');
-            expect(users).toHaveLength(1);
-        });
-
-        it('should delete user', async () => {
-            await Users.remove(1)
-            const users = await db('users');
-            expect(users).toHaveLength(0);
-        });
-
-        // describe('login', () => {
-        //     it('should return a status of 200', () => {
-        //         const res = await request(server).get('/api/auth/login');
-        //         expect(res.status).toBe(200);
-        //     })
-        // })
-    });
-
-    describe('GET Jokes', () => {
-        // it('should return a status of 200', async () => {
-        //     // await Users.add({username: "matt", password: "password" });
-        //     const res = await request(server).get('/api/jokes');
-        //     expect(res.status).toBe(200);
+        // it('should insert user', async () => {
+        //     await Users.add({username:"matthew", password: "password"});
+        //     const users = await db('users');
+        //     expect(users).toHaveLength(1);
         // });
 
-        it('should return json', async () => {
-            const res = await request(server).get('/api/jokes');
-            expect(res.type).toBe('application/json');
+        // it('should delete user', async () => {
+        //     await Users.remove(2)
+        //     const users = await db('users');
+        //     expect(users).toHaveLength(1);
+        // });
+
+
+    });
+    describe('login', () => {
+        it('should return a status of 201', async () => {
+            const res = await request(server).post('/api/auth/register').send({username: 'username', password: 'password'});
+            expect(res.status).toBe(201);
         });
+
+        it('should return a status of 200', async () => {
+            const res = await request(server).post('/api/auth/login').send(user);
+            expect(res.status).toBe(200);
+        });
+
+
     })
+    // describe('GET Jokes', () => {
+    //     it('should return a status of 200', async () => {
+    //         // await Users.add({username: "matt", password: "password" });
+    //         const res = await request(server).get('/api/jokes').set('Authorization', `Bearer ${token}`);
+    //         expect(res.status).toBe(200);
+    //     });
+
+    //     it('should return json', async () => {
+    //         const res = await request(server).get('/api/jokes');
+    //         expect(res.type).toBe('application/json');
+    //     });
+    // })
 });
 
-// beforeEach(async (done) => {
-//     await db('users').truncate();
+// beforeEach((done) => {
+//     request(server).post('/api/auth/login')
+//         .send({username: 'matt', password: 'password'})
+//         .end((err, response) => {
+//             token = response.body.token; // save the token!
+//             done();
+//         });;
+//     // await db('users').truncate();
+
 //     // request(server).post('/api/auth/register').send({username: "matt", password: "password" });
 //     // done();
 
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,25 +2,25 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
-const bcrypt = require('bcryptjs');
 
-const Users = require('../users/users-model');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const token = req.headers.authorization;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' })
-        }
-      });
+  if (req.decodedJwt) {
+    next();
+  } else if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedJwt) => {
+      if (err) {
+        res.status(401).json({ you: "shall not pass!" });
+      } else {
+        req.decodedJwt = decodedJwt;
+        next();
+      }
+    })
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(401).json({ message: "try again" });
   }
-  res.status(401).json({ you: 'shall not pass!' });
 };
